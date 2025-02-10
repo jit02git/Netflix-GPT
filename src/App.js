ce5891cac8c45d8f77e19cfb222b98e3991ee196
jitesh <jitesh02sep@gmail.com>
2025-02-10 23:37:14 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-500">Hello, Tailwind CSS!</h1>
+    </div>
+  );
+}
